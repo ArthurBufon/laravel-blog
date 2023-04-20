06694ac6325b349c446288fef51af2dd1d6d0ee8
Arthur <arthurbufon2052@gmail.com>
2023-04-20 20:24:32 -0300

    adicionado seeds para os models

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
             <h1>{{ $post->title }}</h1>
 
             <p>
-                By <a href="#">Arthur Bufon</a> in
+                By <a href="#">{{$post->user->name}}</a> in
                 <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
             </p>
 
