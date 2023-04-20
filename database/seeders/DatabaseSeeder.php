57ba3d07f101dfc414f34849e66955b2ed20377b
Arthur <arthurbufon2052@gmail.com>
2023-04-20 20:42:46 -0300

    adicionando factories para os models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet. Non debitis rerum sed omnis repudiandae non officia velit id incidunt voluptas. Sit exercitationem dicta cum molestiae iusto in veniam tenetur aut tempore harum qui reprehenderit tempora aut dignissimos cumque sed nesciunt quibusdam!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet. Non debitis rerum sed omnis repudiandae non officia velit id incidunt voluptas. Sit exercitationem dicta cum molestiae iusto in veniam tenetur aut tempore harum qui reprehenderit tempora aut dignissimos cumque sed nesciunt quibusdam!</p>'
-        ]);
-
     }
 }
